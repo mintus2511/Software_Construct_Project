a6f22b2368f97857d518733d6f9d0f52b5ea7933
leoleo <qp713223@gmail.com>
2026-04-14 21:12:37 +0700

    Fix phosor-icons/react

--- MealCraft/frontend/vite.config.ts
+++ MealCraft/frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import react from "@vitejs/plugin-react";
 import tailwind from "tailwindcss";
 import { defineConfig } from "vite";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,11 +10,6 @@ export default defineConfig({
     css: {
         postcss: {
             plugins: [tailwind()],
-        },
-    },
-    resolve: {
-        alias: {
-            // "@": path.resolve(__dirname, "src"),
         },
     },
 });
